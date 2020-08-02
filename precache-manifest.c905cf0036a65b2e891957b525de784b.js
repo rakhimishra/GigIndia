bb3b4883e689478499cce9079f2a30ee3935209b
rakhimishra <mishrarakhi21@gmail.com>
2020-08-02 23:12:16 +0530

    Updates

---
+++ precache-manifest.c905cf0036a65b2e891957b525de784b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f03d439ec428db2ada9416322d66e14c",
+    "url": "/GigIndia/index.html"
+  },
+  {
+    "revision": "96a96c3268216ace10a4",
+    "url": "/GigIndia/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "09bebdb66c07238e6811",
+    "url": "/GigIndia/static/css/main.fbf8e498.chunk.css"
+  },
+  {
+    "revision": "96a96c3268216ace10a4",
+    "url": "/GigIndia/static/js/2.0619afa5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/GigIndia/static/js/2.0619afa5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "09bebdb66c07238e6811",
+    "url": "/GigIndia/static/js/main.bb1447ec.chunk.js"
+  },
+  {
+    "revision": "00f25a370fec8997cf7f",
+    "url": "/GigIndia/static/js/runtime-main.6c2ab58d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/GigIndia/static/media/logo.5d5d9eef.svg"
+  }
+]);
